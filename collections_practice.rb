4f7f598ee931c8a9d4c776a4ab8411c69c34383a
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-01 22:59:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -62,31 +62,7 @@ def merge_data(keys, data)
   result
 end 
 
-# (:keys) [
-#           {
-#             :first_name => "blake"
-#         },
-#           {
-#             :first_name => "ashley"
-#         }
-#         ]
 
-# (:data) {
-#     [
-#           {
-#             "blake" => {
-#                 :awesomeness => 10,
-#                     :height => "74",
-#                   :last_name => "johnson"
-#             },
-#             "ashley" => {
-#                 :awesomeness => 9,
-#                     :height => 60,
-#                   :last_name => "dubs"
-#             }
-#         }
-#     ]
-#   }
 
 
 
