affd447f210af40153a4844fcc15af96f2bf0e3a
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-01 23:04:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -74,6 +74,34 @@ end
 
 
 
+  # let(:schools) {
+  #   {
+  #     "flatiron school bk" => {
+  #       :location => "NYC"
+  #     },
+  #     "flatiron school" => {
+  #       :location => "NYC"
+  #     },
+  #     "dev boot camp" => {
+  #       :location => "SF"
+  #     },
+  #     "dev boot camp chicago" => {
+  #       :location => "Chicago"
+  #     },
+  #     "general assembly" => {
+  #       :location => "NYC"
+  #     },
+  #     "Hack Reactor" => {
+  #       :location => "SF"
+  #     }
+  #   }
+  # }
+  # let(:organized_schools) {
+  #   {"NYC"=>["flatiron school bk", "flatiron school", "general assembly"],
+  #   "SF"=>["dev boot camp", "Hack Reactor"],
+  #   "Chicago"=>["dev boot camp chicago"]}
+  # }
+
 #   describe '#organize_schools' do
 
 #     it 'organizes the schools by location' do
